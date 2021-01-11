92a4fee066ca97ce4fd7c3b87a861d140194708e
Arwa <marina-arwa@hotmail.com>
2021-01-11 22:55:46 +0200

    Updated remove users

--- controllers/administratorController.js
+++ controllers/administratorController.js
@@ -15,23 +15,23 @@ async function createRequest (name,fn,ln,emaill){
 exports.CreateNewRequest = createRequest
 
 exports.getAllRequests = async function(req, res) {
-    let match = await Request.find({},{name:1,firstName:1,lastName:1,email:1,_id:0})
+    let match = await Request.find({},{name:1,firstName:1,lastName:1,email:1,_id:1})
     return res.send(match);
 
 };
 
 exports.getAllUsers = async function(req, res) {
     //we need to add Firstname, LastName,City,Address
-    let users = await User.find({},{_id:0,name:1,firstName:1,lastName:1,role:1,email:1,status:1})
+    let users = await User.find({},{_id:1,name:1,firstName:1,lastName:1,role:1,email:1,status:1})
     return res.send(users);
 
 };
 
 exports.removeUser = async function(req, res) {
-    let username = req.params.username;
+    let username = req.params.id;
     console.log(username)
     await User
-    .deleteOne({name:username})
+    .deleteOne({_id:username})
     return res.status(200).json({
         message: 'Delete User Successful'
       });
--- routes/administrator.js
+++ routes/administrator.js
@@ -6,7 +6,7 @@ const AdministratorController = require('../controllers/administratorController'
 router.get('/getRequests',AdministratorController.getAllRequests);
 router.post('/approve/:username',AdministratorController.approveRequest);
 router.get('/getUsers',AdministratorController.getAllUsers);
-router.post('/remove/:username',AdministratorController.removeUser);
+router.post('/remove/:id',AdministratorController.removeUser);
 
 
 
