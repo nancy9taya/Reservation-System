42ebe2dd90e636dddc518496d1cde2191e66bf90
Salma Farghaly <SalmaFarghaly131@gmail.com>
2021-01-13 13:49:57 +0200

    commit

--- routes/match.js
+++ routes/match.js
@@ -7,6 +7,6 @@ router.post("/newEvent", MatchController.CreateNewEvent);
 router.post('/editEvent/:id',MatchController.EditExistingEvent);
 router.get('/matchDetails/:id',MatchController.FindExistingEvent);
 router.get('/getinfo/:id',MatchController.FindExistingEvent);
-
+router.get('/allMatches',MatchController.ViewAllEvents);
 
 module.exports = router;
