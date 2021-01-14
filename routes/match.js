abf183ccb3d9922eb1791ae8ddfa3418720923f4
nancy9taya <nancy.hassan1998@gmail.com>
2021-01-14 03:09:57 +0200

    view seats

--- routes/match.js
+++ routes/match.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const MatchController = require('../controllers/matchControllers');
+const checkAuth = require('../middleware/checkAuth');
 
 router.post("/newEvent", MatchController.CreateNewEvent);
 router.post('/editEvent/:id',MatchController.EditExistingEvent);
 router.get('/matchDetails/:id',MatchController.FindExistingEvent);
 router.get('/getinfo/:id',MatchController.FindExistingEvent);
 router.get('/allMatches',MatchController.ViewAllEvents);
+router.post('/allSeats/:id',checkAuth,MatchController.viewAllSeats)
 
 module.exports = router;
